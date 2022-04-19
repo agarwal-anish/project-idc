1835e01fe23349ea44d03efb2b5a272f7328eb80
Anish Agarwal <86550700+agarwal-anish@users.noreply.github.com>
2022-04-19 18:05:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    clown_nose = loadimage("https://i.postimg.cc/5NdG89HG/Clown-nose-image-ha-ha-ha.jpg")
+}
+
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() { 
+    console.log('PoseNet Is Inistialized');
+}
+
+function gotPoses(results){
+if( results.length > 0){
+    console.log(results);
+console.log("nose x = " + results[0].pose.nose.x);
+ console.log("nose y = " + results[0].pose.nose.y);
+}
+}
+
+function draw() {
+    image(video,0,0, 300, 300);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
